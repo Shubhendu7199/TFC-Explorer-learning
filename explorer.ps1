861983f540e502d8f2f0bd389e5d2be680bf5014
Shubhendu Upadhyay <shubhendu_binodkumar@srmuniv.edu.in>
2024-03-18 15:15:41 +0530

    Update explorer.ps1

--- explorer.ps1
+++ explorer.ps1
@@ -2,6 +2,6 @@ param(
     [string]$type
 )
 
-$response = Invoke-WebRequest -Uri "https://app.terraform.io/api/v2/organizations/$env:orgName/explorer/export/csv?type=$type" -Headers @{ "Authorization" = "Bearer $env:token" }
+$response = Invoke-WebRequest -Uri "https://app.terraform.io/api/v2/organizations/$env:orgName/explorer/export/csv?type=workspaces" -Headers @{ "Authorization" = "Bearer $env:token" }
 $content = $response.Content
 $content | Set-Content -Path "output.csv"
